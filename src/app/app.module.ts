76ca1af5a5ffd1bcb3a889e17d1cf203fa3ffade
Miquel Puig <mpuig22@gmail.com>
2019-08-12 15:06:20 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+ 
 import { AppComponent } from './app.component';
-
+//import { HttpModule } from '@angular/https';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';//para hacer un rest
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import 'rxjs';//map
+import { map, delay } from 'rxjs/operators';
+import { HttpClient, HttpClientJsonpModule, JsonpClientBackend, JsonpInterceptor } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+private apikey:string = "9a5db3391a9e2f3731090d14d4bd6943";
+private urlMoviedb:string = "https://api.themoviedb.org/3/";
+//https://api.themoviedb.org/3/movie/550?api_key=9a5db3391a9e2f3731090d14d4bd6943
+
+  constructor( private jsonp:JsonpClientBackend, private http:HttpClient) {  }
+
+  getPopulares(){
+    //return this.http.get(`${this.urlMoviewdb}/discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es`);
+    //.pipe(map( //(res)=> res.json()
+    //.pipe(map( (res)=> res));
+ 
+    return this.http.jsonp(`${this.urlMoviedb}/discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es`, "callback");
+  }    
+}
